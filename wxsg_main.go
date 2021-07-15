e745ff7a617335a9473a5245f29a2d5237a5a41c
Tong Sun <suntong@cpan.org>
2021-07-14 22:39:26 -0400

    - [+] use Qrcode lib for terminal

--- wxsg_main.go
+++ wxsg_main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/skip2/go-qrcode"
 	"github.com/eatMoreApple/openwechat"
 )
 
 func main() {
 	bot := openwechat.DefaultBot(openwechat.Desktop)
+	// 注册登陆二维码回调
+	bot.UUIDCallback = ConsoleQrCode
 
 	// 注册消息处理函数
 	bot.MessageHandler = func(msg *openwechat.Message) {
@@ -15,8 +18,6 @@ func main() {
 			fmt.Println("你收到了一条新的文本消息")
 		}
 	}
-	// 注册登陆二维码回调
-	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
 
 	// 登陆
 	if err := bot.Login(); err != nil {
@@ -41,4 +42,10 @@ func main() {
 
 	// 阻塞主goroutine, 知道发生异常或者用户主动退出
 	bot.Block()
+}
+
+
+func ConsoleQrCode(uuid string) {
+	q, _ := qrcode.New("https://login.weixin.qq.com/l/"+uuid, qrcode.Low)
+	fmt.Println(q.ToString(true))
 }
